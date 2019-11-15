75fab91424dc1006dbb788078c13b10b4e9acdb6
Vinay Gaba <vinaygaba@gmail.com>
2019-11-14 22:27:56 -0800

    Moved to using usecase

--- app/src/main/java/com/droidconsf/architectureagnosticuidevelopment/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/droidconsf/architectureagnosticuidevelopment/viewmodels/MainViewModel.kt
@@ -1,22 +1,20 @@
 package com.droidconsf.architectureagnosticuidevelopment.viewmodels
 
 import com.droidconsf.architectureagnosticuidevelopment.BuildConfig
-import com.droidconsf.architectureagnosticuidevelopment.api.MarvelApi
 import com.droidconsf.architectureagnosticuidevelopment.rx.SchedulersProvider
+import com.droidconsf.architectureagnosticuidevelopment.usecases.GetComics
 import com.droidconsf.architectureagnosticuidevelopment.util.md5
 import java.util.Date
 
-// TODO(vinay) Replace this with a usecase which in turn contains a repository which references the
-// MarvelApi
-class MainViewModel(val marvelApi: MarvelApi,
-                    val schedulersProvider: SchedulersProvider) : BaseViewModel() {
+class MainViewModel(val schedulersProvider: SchedulersProvider,
+                    val getComics: GetComics) : BaseViewModel() {
     init {
         loadComics()
     }
 
     fun loadComics() {
         val timestamp = Date().time.toString()
-        marvelApi.getComics(timestamp, BuildConfig.API_KEY,
+        getComics(timestamp, BuildConfig.API_KEY,
             md5(timestamp + BuildConfig.PRIVATE_KEY + BuildConfig.API_KEY))
             .subscribeOn(schedulersProvider.io())
             .subscribe({},{})
