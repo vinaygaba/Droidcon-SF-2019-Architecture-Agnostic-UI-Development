28ff8e5b9ffe327e6f5e7fe09d38710b6526f917
Vinay Gaba <vinaygaba@gmail.com>
2019-11-17 08:33:41 -0800

    Setup MarvelApi along with a functioning getComics endpoint (#1)
    * Stashing changes
    
    * Stashing changes
    
    * Added api
    
    * Nit

---
+++ app/src/main/java/com/droidconsf/architectureagnosticuidevelopment/api/MarvelApi.kt
@@ -0,0 +1,15 @@
+package com.droidconsf.architectureagnosticuidevelopment.api
+
+import com.droidconsf.architectureagnosticuidevelopment.api.models.MarvelResponses
+import io.reactivex.Single
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MarvelApi {
+
+    @GET("v1/public/comics")
+    fun getComics(@Query("ts") ts: String,
+                  @Query("apikey") apiKey: String,
+                  @Query("hash") hash: String): Single<Response<MarvelResponses>>
+}
