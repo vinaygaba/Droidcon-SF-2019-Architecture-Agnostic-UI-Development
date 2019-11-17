4b2f9ea172f3565a41e204b435d9d99bf5ee1f90
Vinay Gaba <vinaygaba@gmail.com>
2019-11-17 08:34:52 -0800

    Integrated Flipper with the app (#2)
    * Stashing changes
    
    * Stashing changes
    
    * Added api
    
    * Nit
    
    * Added Flipper
    
    * Add relevant dependencies

---
+++ app/src/main/java/com/droidconsf/architectureagnosticuidevelopment/ArchitectureAgnosticUiApplication.kt
@@ -0,0 +1,23 @@
+package com.droidconsf.architectureagnosticuidevelopment
+
+import android.app.Application
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
+
+
+class ArchitectureAgnosticUiApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.addPlugin(NetworkFlipperPlugin())
+            client.start()
+        }
+    }
+}
