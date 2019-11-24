6027a9fe30443b0ec84b7823a4a38e152a31decf
Vinay Gaba <vinaygaba@gmail.com>
2019-11-23 21:16:27 -0800

    Minor changes

--- app/src/main/java/com/droidconsf/architectureagnosticuidevelopment/ui/comicbooks/compose/ComicDetailScreen.kt
+++ app/src/main/java/com/droidconsf/architectureagnosticuidevelopment/ui/comicbooks/compose/ComicDetailScreen.kt
@@ -8,6 +8,7 @@ import androidx.ui.core.dp
 import androidx.ui.foundation.Clickable
 import androidx.ui.graphics.Color
 import androidx.ui.layout.*
+import androidx.ui.material.surface.Surface
 import androidx.ui.material.themeTextStyle
 import androidx.ui.text.style.TextOverflow
 import com.droidconsf.architectureagnosticuidevelopment.core.api.models.Comic
@@ -21,15 +22,17 @@ internal fun ComicDetailScreen(
     descriptionExpanded: Boolean?
 ) {
     comic?.let {
-        Container(modifier = Spacing(16.dp), alignment = Alignment.TopCenter) {
-            FlexRow {
-                inflexible {
-                    ComicImage(122.dp, 170.dp)
-                }
-                expanded(1f) {
-                    Column {
-                        ComicTitleAndDescription(it, descriptionExpanded)
-                        ShowMoreButton(viewModel, descriptionExpanded)
+        Surface(color = Color(24, 24, 24)) {
+            Container(modifier = Spacing(16.dp), alignment = Alignment.TopCenter) {
+                FlexRow {
+                    inflexible {
+                        ComicImage(122.dp, 170.dp)
+                    }
+                    expanded(1f) {
+                        Column {
+                            ComicTitleAndDescription(it, descriptionExpanded)
+                            ShowMoreButton(viewModel, descriptionExpanded)
+                        }
                     }
                 }
             }
@@ -44,12 +47,12 @@ internal fun ComicTitleAndDescription(
 ) {
     val maxLines = if (descriptionExpanded == true) 12 else 3
     Padding(left = 16.dp, right = 16.dp, bottom = 16.dp) {
-        val textStyle = (+themeTextStyle { subtitle1 }).copy(color = Color.Black)
+        val textStyle = (+themeTextStyle { subtitle1 }).copy(color = Color.White)
         Text(comic.title, style = textStyle, maxLines = 1, overflow = TextOverflow.Ellipsis)
     }
     comic.description?.let { sub ->
         Padding(left = 16.dp, right = 16.dp) {
-            val textStyle = (+themeTextStyle { caption }).copy(color = Color.Black)
+            val textStyle = (+themeTextStyle { caption }).copy(color = Color.White)
             Text(sub, style = textStyle, maxLines = maxLines, overflow = TextOverflow.Ellipsis)
         }
     }
@@ -61,9 +64,9 @@ internal fun ShowMoreButton(
     descriptionExpanded: Boolean?
 ) {
     if (descriptionExpanded != true) {
-        val buttonStyle = (+themeTextStyle { h6 }).copy(color = Color.Black)
+        val buttonStyle = (+themeTextStyle { h6 }).copy(color = Color.White)
         Center {
-            Padding(left = 16.dp, right = 16.dp) {
+            Padding(left = 16.dp, right = 16.dp, top = 16.dp) {
                 Clickable(onClick = {
                     viewModel.triggerEvent(Event.View.ShowMoreDescription)
                 }) {
